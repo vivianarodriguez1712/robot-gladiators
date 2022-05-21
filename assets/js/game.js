b89bfeb4547165a20c19238352183370d37c0c3f
viviana rodriguez <vivianaprodriguez@live.com>
2022-05-21 19:09:34 -0400

    added a new enemyName code

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,15 +7,18 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
 var fight = function() {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
 
+  console.log(enemyNames);
+  // Alert players that they are starting the round
+  
+  window.alert("Welcome to Robot Gladiators!");
+  console.log(enemyNames[0]);
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
