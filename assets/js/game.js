989997f71140c1a0c28a75043aea0538b61a1f9f
viviana rodriguez <vivianaprodriguez@live.com>
2022-05-22 18:30:27 -0400

    added math codes

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,14 +22,14 @@ var fight = function(enemyName) {
     if (confirmSkip) {
       window.alert(playerName + ' has decided to skip this fight. Goodbye!');
       // subtract money from playerMoney for skipping
-      playerMoney = playerMoney - 10;
+      playerMoney = Math.max(0, playerMoney - 10);
       console.log("playerMoney", playerMoney);
       break;
     }
   }
 
   // remove enemy's health by subtracting the amount set in the playerAttack variable
-  enemyHealth = enemyHealth - playerAttack;
+  enemyHealth = Math.max(0, enemyHealth - playerAttack);
   console.log(
     playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
   );
@@ -48,7 +48,7 @@ var fight = function(enemyName) {
   }
 
   // remove players's health by subtracting the amount set in the enemyAttack variable
-  playerHealth = playerHealth - enemyAttack;
+  playerHealth = Math.max(0, playerHealth - enemyAttack);
   console.log(
     enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
   );
@@ -82,7 +82,7 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = Math.floor(Math.random() * 21) + 40;
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
@@ -187,3 +187,19 @@ switch (shopOptionPrompt) {
 
 // start the game when the page loads
 startGame();
+
+// prints 3.141592653589793
+console.log(Math.PI);
+
+ // rounds to the nearest whole number (4)
+console.log(Math.round(4.4));
+
+ // prints the square root (5)
+console.log(Math.sqrt(25));
+
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
