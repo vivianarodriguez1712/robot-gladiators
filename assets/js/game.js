9744e38cfe6750fbee467938d4f5ddc5c8e3b3a9
viviana rodriguez <vivianaprodriguez@live.com>
2022-05-23 00:20:20 -0400

    updating debugger code

--- assets/js/game.js
+++ assets/js/game.js
@@ -123,8 +123,8 @@ var startGame = function() {
     // if player is still alive, keep fighting
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-      
+      window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+      debugger;
       // pick new enemy to fight based on the index of the enemyNames array
       var pickedEnemyObj = enemyInfo[i];
       // reset enemyHealth before starting new fight
@@ -143,8 +143,6 @@ var startGame = function() {
           shop();
         }
       }
-     window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-      debugger;
     }
 
     else {
